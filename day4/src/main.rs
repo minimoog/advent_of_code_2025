68e65eec25bb10b826ec48bf129d96c219fd88fe
Toni Jovanoski <toni.jovanoski@hubsecurity.io>
2025-12-15 20:14:04 +0100

    Solve day4 part one.

---
+++ day4/src/main.rs
@@ -0,0 +1,59 @@
+use std::{fs::File, io::{self, BufRead}};
+
+mod matrix;
+
+use matrix::Matrix;
+
+const ROWS: usize = 137;
+const COLS: usize = 137;
+
+fn main() -> std::io::Result<()> {
+    let mut count1 = 0;
+
+    let file = File::open("puzzle.input")?;
+
+    let reader = io::BufReader::new(file);
+
+    let mut grid = Matrix::<char>::new(ROWS, COLS);
+
+    for (idy, line) in reader.lines().enumerate() {
+        match line {
+            Ok(content) => {
+                //println!("{}, {}", idx, content);
+
+                for (idx, c) in content.chars().enumerate() {
+                    grid[(idx, idy)] = c;
+                }
+
+            },
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
+
+    for y in 0..ROWS {
+        for x in 0..COLS {
+            if grid[(x, y)] == '.' { continue; }
+
+            let n = grid.neighbors8(x, y);
+
+            let mut ccounter = 0;
+
+            for (_, _, c) in n {
+                if *c == '@' { ccounter += 1; }
+            }
+
+            if ccounter < 4 {
+                count1 += 1;
+
+                println!("{} {}", x, y);
+            }
+        }
+
+    }
+
+    println!("{}", count1);
+
+    Ok(())
+}
