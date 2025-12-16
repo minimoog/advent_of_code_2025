140f5f246a5bd2427bd9de1913c1d3320f76f4ef
Toni Jovanoski <toni.jovanoski@hubsecurity.io>
2025-12-16 11:13:53 +0100

    Solve part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,8 +7,38 @@ use matrix::Matrix;
 const ROWS: usize = 137;
 const COLS: usize = 137;
 
+fn removable_papers(grid: &Matrix<char>) -> Vec<(usize, usize)>
+{
+    let mut output: Vec<(usize, usize)> = Vec::new();
+
+    for y in 0..ROWS {
+        for x in 0..COLS {
+            if grid[(x, y)] == '.' { continue; }
+
+            let counter = grid.neighbors8(x, y).into_iter()
+                .filter(|&x| *x.2 == '@')
+                .count();
+
+            if counter < 4 { 
+                output.push((x, y));
+            }
+        }
+
+    }
+
+    output
+}
+
+fn remove_papers(grid: &mut Matrix<char>, toremove: Vec<(usize, usize)>)
+{
+    for (x, y) in toremove {
+        grid[(x, y)] = '.';
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let mut count1 = 0;
+    let mut count2 = 0;
 
     let file = File::open("puzzle.input")?;
 
@@ -32,6 +62,7 @@ fn main() -> std::io::Result<()> {
         }
     }
 
+    //part 1
     for y in 0..ROWS {
         for x in 0..COLS {
             if grid[(x, y)] == '.' { continue; }
@@ -45,7 +76,23 @@ fn main() -> std::io::Result<()> {
 
     }
 
-    println!("{}", count1);
+    println!("part1: {}", count1);
+
+    //part2
+
+    loop {
+        let removable = removable_papers(&grid);
+
+        if removable.len() == 0 {
+            break;
+        }
+
+        count2 += removable.len();
+
+        remove_papers(&mut grid, removable);
+    }
+
+    println!("part2: {}", count2);
 
     Ok(())
 }
