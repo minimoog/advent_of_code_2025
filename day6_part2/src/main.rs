d297e0429b3c453225e38cd45551e972cafa1d75
Toni Jovanoski <toni.jovanoski@hubsecurity.io>
2025-12-20 19:17:35 +0100

    Solve day6 part2

--- day6_part2/src/main.rs
+++ day6_part2/src/main.rs
@@ -8,20 +8,27 @@ struct Interval {
 }
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("test.input")?;
+    let file = File::open("puzzle.input")?;
     let mut reader = io::BufReader::new(file);
 
-    let mut line1 = String::new();
-    let mut line2 = String::new();
-    let mut line3 = String::new();
+    let mut line1_string = String::new();
+    let mut line2_string = String::new();
+    let mut line3_string = String::new();
+    let mut line4_string = String::new();
     let mut ops_line = String::new();
 
-    let _ = reader.read_line(&mut line1).unwrap();
-    let _ = reader.read_line(&mut line2).unwrap();
-    let _ = reader.read_line(&mut line3).unwrap();
+    let _ = reader.read_line(&mut line1_string).unwrap();
+    let _ = reader.read_line(&mut line2_string).unwrap();
+    let _ = reader.read_line(&mut line3_string).unwrap();
+    let _ = reader.read_line(&mut line4_string).unwrap();
     let _ = reader.read_line(&mut ops_line).unwrap();
 
-    println!("{}{}{}{}", line1, line2, line3, ops_line);
+    let line1 = line1_string.as_bytes();
+    let line2 = line2_string.as_bytes();
+    let line3 = line3_string.as_bytes();
+    let line4 = line4_string.as_bytes();
+
+    println!("{}{}{}{}", line1_string, line2_string, line3_string, line4_string);
 
     let mut intervals: Vec<Interval> = Vec::new();
 
@@ -46,7 +53,52 @@ fn main() -> std::io::Result<()> {
         last.higher += 1;
     }
 
-    for i in intervals { println!("{} {} {}", i.lower, i.higher, i.ops); }
+    let mut part2 = 0;
+
+    for interval in intervals {
+        let mut sum_or_prod = 0;
+
+        if interval.ops == '*' { sum_or_prod = 1; }
+
+        for i in interval.lower..=interval.higher {
+            let mut n: u64 = 0;
+
+
+            if line1[i] != b' ' {
+                println!("{}", line1[i]);
+                n *= 10;
+                n += (line1[i] - b'0') as u64;
+            }
+
+            if line2[i] != b' ' {
+                n *= 10;
+                n += (line2[i] - b'0') as u64;
+            }
+
+            if line3[i] != b' ' {
+                n *= 10;
+                n += (line3[i] - b'0') as u64;
+            }
+
+            if line4[i] != b' ' {
+                n *= 10;
+                n += (line4[i] - b'0') as u64;
+            }
+
+            if interval.ops == '+' {
+                sum_or_prod += n;
+            } else {
+                sum_or_prod *= n;
+            }
+        }
+
+        //println!("sum or prod {}", sum_or_prod);
+
+        part2 += sum_or_prod;
+
+    }
+
+    println!("{}", part2);
 
     Ok(())
 }
